fc98809ce1c13ab4ebbe9510c436853f16d8cfad
Francisco José Rodríguez Bogado <bogado@qinn.es>
2017-09-15 15:17:50 +0200

    Paso 2: Renderizando el mapa.

--- localización/README.md
+++ localización/README.md
@@ -2,7 +2,27 @@
 
 Aplicación para geolocalizar el móvil, mostrar un mapa y guardar lugares favoritos.
 
+## Paso 1
 
+Obtener las coordenadas de la posición del móvil.
+
+## Paso 2
+
+Dibujar un mapa según las coordenadas del paso anterior. La porción del mapa a dibujar se descargará de [mapbox](http://www.mapbox.com/) usando la biblioteca [leaflet.zip](http://leafletjs.com/examples/quick-start/).
+
+Para acceder a los mapas es necesario registrarse en Mapbox y generar un _Access Token_ que se usa en la función `dibujaCoordenadas` de `www/js/maps.js`. El _Access Token_ debe guardarse en el fichero `www/js/config.js` —no incluido en el repositorio—, que contendría:
+
+```
+var config = {
+    MAPBOX_APIKEY: 'pk.eyTUACCESSTOKENAQUItuaccesstokenaquiTUACCESSTOKENAQUItuaccesstokenaqui.Mlajkshdan'
+}
+```
+
+## Paso 3
+
+---
+
+![Captura de pantalla](www/img/Screenshot_2017-09-15-15-15-30.png)
 
 Servir la aplicación desde el ordenador con
 ```
--- localización/www/js/maps.js
+++ localización/www/js/maps.js
@@ -8,12 +8,26 @@ var app = {
     },
 
     dispositivoListo: function(){
+        console.log("¡Listo!");
         navigator.geolocation.getCurrentPosition(app.dibujaCoordenadas, app.errorAlSolicitarLocalizacion);
     },
 
     dibujaCoordenadas: function(position){
         var coordsDiv = document.querySelector('#coords');
         coordsDiv.innerHTML = 'Latitud: ' + position.coords.latitude + ' Longitud:' + position.coords.longitude;
+        console.log("Posición localizada");
+        var mi_mapa = L.map('map').setView([position.coords.latitude, position.coords.longitude], 13);
+        console.log("Accediendo a mapbox...");
+        L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}',
+        //L.tileLayer('https://api.tiles.mapbox.com/v4/MapID/997/256/{z}/{x}/{y}.png?access_token={accessToken}',
+        //L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?access_token={accessToken}',
+            {attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+             maxZoom: 13,
+             id: 'mapbox.streets',
+             //id: 'mapbox.satellite',
+             accessToken: config.MAPBOX_APIKEY}
+        ).addTo(mi_mapa);
+        console.log("Mapa renderizado.");
     },
 
     errorAlSolicitarLocalizacion: function(error){
